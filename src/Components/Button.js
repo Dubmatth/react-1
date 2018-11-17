c7115d8f79ff3ba5b62e5fe343d53bcfcf9b7b68
Dubois Matthieu <dubois.matthieu@live.be>
2018-11-17 22:00:08 +0100

    Ajout du fichier button.css & application de style 'Inline'.

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './Button.css';
+
 
 const Button = ({vieillir}) => (
     <button onClick={vieillir}>
--- src/Components/Membre.js
+++ src/Components/Membre.js
@@ -3,7 +3,12 @@ import React, { Fragment } from 'react'
 const Membre = ({ nom, age, children }) => {
     return (
         <Fragment>
-            <h2>{nom.toUpperCase()} : {age}</h2>
+            <h2 style={{
+                backgroundColor: age < 10 ? 'grey' : 'purple', 
+                color: age < 10 ? 'white' : 'black'
+            }}>
+                {nom.toUpperCase()} : {age}
+            </h2>
             { children ? <p>{children}</p> : <Fragment/>}
         </Fragment>
     )
